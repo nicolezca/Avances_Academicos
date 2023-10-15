a9803a07e4378685b3216ac8414be46002525814
nicolezca <tatilezcano03@gmail.com>
2023-10-15 19:49:07 -0300

    se le agrego un data-target a cada card

--- partials/navBar.php
+++ partials/navBar.php
@@ -3,11 +3,11 @@
         <span>INDUSTRIAL N°1</span>
     </div>
     <nav class="link">
-        <div class="cards active"><a href="#"><i class='bx bx-home'></i><span>INCIO</span></a></div>
-        <div class="cards"><a href="#"><i class='bx bx-user-circle'></i><span>PROFESORES</span></a></div>
-        <div class="cards"><a href="#"><i class='bx bx-book'></i><span>MATERIAS</span></a></div>
-        <div class="cards"><a href="#"><i class='bx bx-group'></i><span>ALUMNOS</span></a></div>
-        <div class="cards"><a href="#"><i class='bx bx-chalkboard'></i><span>CURSO</span></a></div>
+        <div class="cards active"  data-target="inicio"><a href="#"><i class='bx bx-home'></i><span>INCIO</span></a></div>
+        <div class="cards"  data-target="profesores"><a href="#"><i class='bx bx-user-circle'></i><span>PROFESORES</span></a></div>
+        <div class="cards"  data-target="materias"><a href="#"><i class='bx bx-book'></i><span>MATERIAS</span></a></div>
+        <div class="cards"  data-target="alumnos"><a href="#"><i class='bx bx-group'></i><span>ALUMNOS</span></a></div>
+        <div class="cards"  data-target="curso"><a href="#"><i class='bx bx-chalkboard'></i><span>CURSO</span></a></div>
         <div class="cards exit"><a href="#"><i class='bx bx-door-open'></i><span>SALIR</span></a></div>
     </nav>
 </div>
