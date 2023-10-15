89e3609561830052c3355787820e957a09c5f7f5
nicolezca <tatilezcano03@gmail.com>
2023-10-14 21:02:38 -0300

    archivo de la barra de navegacion

---
+++ partials/navBar.php
@@ -0,0 +1,13 @@
+<div class="navbar">
+    <div class="logo"><img src="../assets/logo_escuela.png" alt="">
+        <span>INDUSTRIAL N°1</span>
+    </div>
+    <nav class="link">
+        <div class="cards"><i class='bx bx-home'></i><span>INCIO</span></div>
+        <div class="cards"><i class='bx bx-user-circle'></i><span>PROFESORES</span></div>
+        <div class="cards"><i class='bx bx-book'></i><span>MATERIAS</span></div>
+        <div class="cards"><i class='bx bx-group'></i><span>ALUMNOS</span></div>
+        <div class="cards"><i class='bx bx-chalkboard'></i><span>CURSO</span></div>
+        <div class="cards exit"><i class='bx bx-door-open'></i><span>SALIR</span></div>
+    </nav>
+</div>
