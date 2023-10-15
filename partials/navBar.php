aadabc9eae14e9ef9e87e6eae4af553690e82166
nicolezca <tatilezcano03@gmail.com>
2023-10-14 22:00:18 -0300

    se agrego links y clase active a las cards

--- partials/navBar.php
+++ partials/navBar.php
@@ -3,11 +3,11 @@
         <span>INDUSTRIAL N°1</span>
     </div>
     <nav class="link">
-        <div class="cards"><i class='bx bx-home'></i><span>INCIO</span></div>
-        <div class="cards"><i class='bx bx-user-circle'></i><span>PROFESORES</span></div>
-        <div class="cards"><i class='bx bx-book'></i><span>MATERIAS</span></div>
-        <div class="cards"><i class='bx bx-group'></i><span>ALUMNOS</span></div>
-        <div class="cards"><i class='bx bx-chalkboard'></i><span>CURSO</span></div>
-        <div class="cards exit"><i class='bx bx-door-open'></i><span>SALIR</span></div>
+        <div class="cards active"><a href="#"><i class='bx bx-home'></i><span>INCIO</span></a></div>
+        <div class="cards"><a href="#"><i class='bx bx-user-circle'></i><span>PROFESORES</span></a></div>
+        <div class="cards"><a href="#"><i class='bx bx-book'></i><span>MATERIAS</span></a></div>
+        <div class="cards"><a href="#"><i class='bx bx-group'></i><span>ALUMNOS</span></a></div>
+        <div class="cards"><a href="#"><i class='bx bx-chalkboard'></i><span>CURSO</span></a></div>
+        <div class="cards exit"><a href="#"><i class='bx bx-door-open'></i><span>SALIR</span></a></div>
     </nav>
 </div>
