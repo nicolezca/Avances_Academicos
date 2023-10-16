2394a72626de35daa5898d829b030b4c2a4b915f
nicolezca <tatilezcano03@gmail.com>
2023-10-16 16:20:21 -0300

    se alojo el archivo filter en section

---
+++ partials/section/filter.php
@@ -0,0 +1,10 @@
+<nav id="filterNav">
+    <div class="agregar">
+        <button id="mostrarFormulario">+</button>
+    </div>
+    <div class="filtrar">
+        <i class='bx bx-filter-alt'></i>
+        <input type="search" name="filter" id="filter" placeholder="buscar nombre">
+        <button id="aplicarFiltro">Aplicar Filtro</button>
+    </div>
+</nav>
