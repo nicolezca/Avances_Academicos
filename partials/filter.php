3afca726f4717e8ba9f6d32cd3b2de3a9d40b33b
nicolezca <tatilezcano03@gmail.com>
2023-10-16 03:08:02 -0300

    archivo del filtro

---
+++ partials/filter.php
@@ -0,0 +1,11 @@
+<nav id="filterNav">
+    <div class="agregar">
+        <button id="mostrarFormulario">+</button>
+    </div>
+    <div class="filtrar">
+        <i class='bx bx-filter-alt'></i>
+        <input type="search" name="filter" id="filter" placeholder="buscar nombre">
+        <button id="aplicarFiltro">Aplicar Filtro</button>
+    </div>
+</nav>
+<script src="../inicio/js/filter.js"></script>
