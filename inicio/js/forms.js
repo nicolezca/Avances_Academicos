1b84280efc89d6cbbeac6bd27eb02ec970608816
nicolezca <tatilezcano03@gmail.com>
2023-10-16 17:44:06 -0300

    se agrego contantes y funciones repetidas para x formularios

--- inicio/js/forms.js
+++ inicio/js/forms.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const btnform = document.getElementById("verformulario");
-    const formulario = document.querySelector(".formulario");
-
+    const formulario = document.getElementById("form-alumno");
+    const btnform2 = document.getElementById("verformulario2");
+    const formularioDocente = document.getElementById("form-docente")
+    formularioDocente.style.left="500%"    
     formulario.style.left="500%";
-
+    
     btnform.addEventListener("click", function () {
         if (formulario.style.left === "500%") {
             formulario.style.left = "60%";
         } else {
             formulario.style.left = "500%";
+        }
+    });
+    
+
+
+    btnform2.addEventListener("click", function () {
+        if (formularioDocente.style.left === "500%") {
+            formularioDocente.style.left = "60%";
+        } else {
+            formularioDocente.style.left = "500%";
         }
     });
 
