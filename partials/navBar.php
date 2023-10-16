3adce614897263ef957d0f7e9e532c98408e628d
nicolezca <tatilezcano03@gmail.com>
2023-10-16 16:19:31 -0300

    se elemino el sapn sobrante

--- partials/navBar.php
+++ partials/navBar.php
@@ -3,11 +3,11 @@
         <span>INDUSTRIAL N°1</span>
     </div>
     <nav class="link">
-        <div class="cards active"  data-target="inicio"><a href="#"><i class='bx bx-home'></i><span>INCIO</span></a></div>
-        <div class="cards"  data-target="profesores"><a href="#"><i class='bx bx-user-circle'></i><span>PROFESORES</span></a></div>
-        <div class="cards"  data-target="materias"><a href="#"><i class='bx bx-book'></i><span>MATERIAS</span></a></div>
-        <div class="cards"  data-target="alumnos"><a href="#"><i class='bx bx-group'></i><span>ALUMNOS</span></a></div>
-        <div class="cards"  data-target="curso"><a href="#"><i class='bx bx-chalkboard'></i><span>CURSO</span></a></div>
-        <div class="cards exit"><a href="#"><i class='bx bx-door-open'></i><span>SALIR</span></a></div>
+        <div class="cards active"  data-target="inicio"><a href="#"><i class='bx bx-home'></i>INCIO</a></div>
+        <div class="cards"  data-target="profesores"><a href="#"><i class='bx bx-user-circle'></i>PROFESORES</a></div>
+        <div class="cards"  data-target="materias"><a href="#"><i class='bx bx-book'></i>MATERIAS</a></div>
+        <div class="cards"  data-target="alumnos"><a href="#"><i class='bx bx-group'></i>ALUMNOS</a></div>
+        <div class="cards"  data-target="curso"><a href="#"><i class='bx bx-chalkboard'></i>CURSO</a></div>
+        <div class="cards exit"><a href="#"><i class='bx bx-door-open'></i>SALIR</a></div>
     </nav>
 </div>
