7a1c1be020968fc145b23875b0fd155a19a493d9
nicolezca <tatilezcano03@gmail.com>
2023-11-07 07:53:28 -0300

    se creo el archivo de inicio de sesion

---
+++ login/login.php
@@ -0,0 +1,30 @@
+<?php
+
+include('../database/conexion.php');
+// Procesar registro
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nombre"]) && isset($_POST["correo"])  && isset($_POST["clave"])) {
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $clave = $_POST["clave"];
+
+    // Validar los campos de registro
+    if (empty($nombre) || empty($correo) || empty($clave)) {
+        echo "Por favor, complete todos los campos.";
+    } else {
+        // Verificar si el nombre ya está registrado en la base de datos
+        $sql = "SELECT * FROM usuarios WHERE correo = '$correo'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows  == 1) {
+            header("Location: ../inicio/home.php");
+            exit();
+        } else {
+            echo "El usuario no esta registrado Por favor verifique su informacion: " . $conn->error;
+            echo '<a href="../formulario.html">Volver a intentar</a>';
+        }
+    }
+}
+
+// Cerrar la conexión a la base de datos
+$conn->close();
+?>
