7d5daa168e253c242eb8ab9dc61aa23b75d7b10b
nicolezca <tatilezcano03@gmail.com>
2023-11-07 08:40:29 -0300

    el archivo se elimino

--- partials/section/inicio.php
+++
@@ -1,8 +0,0 @@
-<h1>ESCUELA SECUNDARIA TECNICA N°1</h1>
-        <P>La institución es una escuela técnica urbana de gestión pública ubicada en el Municipio de Chascomus dependiente de la región educativa 17 de la Provincia de Buenos Aires.</P>
-        <P>Brinda servicios educativos de gestión estatal en la modalidad de educación común en el termino de secundario técnico avalado por el Instituto Nacional de Educación Tecnológica (INET).</P>
-
-        <div class="ubicacion">
-            <h2>UBICACION GEOGRAFICA</h2>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6920.862929495881!2d-58.017429431702986!3d-35.568571472264324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95980da6a7f988bd%3A0x8fe4f82a3d73fc49!2sEscuela%20Industrial%20Chascomus!5e0!3m2!1ses!2sar!4v1697229690087!5m2!1ses!2sar" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
