98d7085d69b8f5b0be7dc0ace9a40958ae428218
nicolezca <tatilezcano03@gmail.com>
2023-11-07 08:38:20 -0300

    se simplifico  la funcionalidad del formulario

--- inicio/js/forms.js
+++ inicio/js/forms.js
@@ -1,36 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function toggleFormulario(formulario, boton) {
-        if (formulario.style.left === "500%") {
-            formulario.style.left = "60%";
+
+    const VerFormulario = document.getElementById('verformulario');
+    const Formulario = document.getElementById('formSubir');
+
+    Formulario.style.left = "500%";
+    VerFormulario.addEventListener('click', () => {
+        if (Formulario.style.left === "500%") {
+            Formulario.style.left = "60%";
         } else {
-            formulario.style.left = "500%";
+            Formulario.style.left = "500%";
         }
     }
-
-    const formularios = [
-        {
-            botonId: "verformulario",
-            formularioId: "form-alumno",
-        },
-        {
-            botonId: "verformulario2",
-            formularioId: "form-docente",
-        },
-        {
-            botonId: "verformulario3",
-            formularioId: "form-materias",
-        },
-        {
-            botonId: "verformulario4",
-            formularioId: "form-curso",
-        },
-    ];
-
-    formularios.forEach((element) => {
-        const formulario = document.getElementById(element.formularioId);
-        const boton = document.getElementById(element.botonId);
-
-        formulario.style.left = "500%";
-        boton.addEventListener("click", () => toggleFormulario(formulario, boton));
-    });
-});
+    )
+}
+);
