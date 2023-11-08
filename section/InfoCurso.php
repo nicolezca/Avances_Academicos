598ac28f243ca1a4d88fd7396f22fcad0b6b9575
nicolezca <tatilezcano03@gmail.com>
2023-11-08 08:51:13 -0300

    Archivo donde estara la Informacion del Curso correspondiente

---
+++ section/InfoCurso.php
@@ -0,0 +1,15 @@
+<?php 
+
+include('../database/conexion.php');
+
+
+
+?>
+
+<!-- aca se mostrara la informacion de todo el curs, si bien tendra que mostrar:
+
+-1 preceptor
+-1 profesor
+-alumnos
+
+-->
