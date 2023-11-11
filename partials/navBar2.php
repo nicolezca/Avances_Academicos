8d60e1ba7d3052348a6acd253f9e09fb73517983
nicolezca <tatilezcano03@gmail.com>
2023-11-11 18:05:38 -0300

    codigo de navegacion HTML

---
+++ partials/navBar2.php
@@ -0,0 +1,11 @@
+<div class="navBar2" style="display: flex;justify-content: space-between;align-items: center;">
+    <div class="logo" style="display: flex;align-items: center;">
+        <img src="../assets/logo_escuela.png" alt="" width="30px" height="30px">
+        <span>INDUSTRIAL</span>
+    </div>
+    <nav class="navegacion">
+        <a href="../section/curso.php" id="volver">Volver Atras</a>
+        <button id="VerFormPersonal">Agregar Personal</button>
+        <button id="VerFormAlumno">Agregar Alumno</button>
+    </nav>
+</div>
