a0b29853af6a45c25ef2340cfe6578db80dc2064
nicolezca <tatilezcano03@gmail.com>
2023-11-12 04:38:47 -0300

    Archivo de Extraccion de datos del curso

---
+++ database/Cursoinfo.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $idSala = $_POST["idSala"];
+
+    // Realiza la conexión
+    include('conexion.php');
+
+    $sql = "SELECT id, año, division FROM curso WHERE id = $idSala";
+    $resultado = $conn->query($sql);
+
+    // Verifica si la consulta se ejecutó correctamente
+    if ($resultado) {
+        $fila = $resultado->fetch_assoc();
+
+        // Verifica si se obtuvo una fila y si las columnas tienen valores
+        if ($fila && isset($fila['año'], $fila['division'])) {
+            $idCurso = $fila['id'];
+            $añoCurso = $fila['año'];
+            $divisionCurso = $fila['division'];
+            $resultado->free_result();
+            $conn->close();
+            
+            $_SESSION["idCurso"] = $idCurso;
+            $_SESSION["año"] = $añoCurso;
+            $_SESSION["division"] = $divisionCurso;
+
+            header('Location: ../section/infoCurso.php');
+            exit();
+        } 
+    } 
+}
+?>
