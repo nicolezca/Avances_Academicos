5c87b603adcb173f14490b5a329bdb8534cea3b9
nicolezca <tatilezcano03@gmail.com>
2023-11-14 03:19:09 -0300

    Se elimina card de materias

--- partials/navBar.php
+++ partials/navBar.php
@@ -6,7 +6,6 @@
     <nav class="link">
         <div class="cards"><a href="../inicio/home.php"><i class='bx bx-home'></i>INCIO</a></div>
         <div class="cards"><a href="../section/profesores.php"><i class='bx bx-user-circle'></i>PROFESORES</a></div>
-        <div class="cards"><a href="../section/materias.php"><i class='bx bx-book'></i>MATERIAS</a></div>
         <div class="cards"><a href="../section/alumnos.php"><i class='bx bx-group'></i>ALUMNOS</a></div>
         <div class="cards"><a href="../section/curso.php"><i class='bx bx-chalkboard'></i>CURSO</a></div>
         <div class="cards exit" id="verCardExit"><i class='bx bx-door-open'></i>SALIR</div>
