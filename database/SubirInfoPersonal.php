bec5bed3709cf01a7c9bd9a8cdb0ce68c76d6ce6
nicolezca <tatilezcano03@gmail.com>
2023-11-14 03:18:25 -0300

    Archivo para cargar los Docentes por curso

---
+++ database/SubirInfoPersonal.php
@@ -0,0 +1,19 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombreProfesor = $_POST["docente"];
+        $nombrePreceptor = $_POST["preceptor"];
+        $CursoID = $_POST["idCurso"];
+
+        include('conexion.php');
+
+        // Inserta la información en la tabla Alumnos
+        $sql = "INSERT INTO cursopersonal (ID_Curso, ID_Preceptor, ID_Profesor) VALUES ( $CursoID, $nombrePreceptor, $nombreProfesor)";
+        $resultado = $conn->query($sql);
+
+        // Cierra la conexión a la base de datos
+        $conn->close();
+
+        header('Location: ../section/infoCurso.php');
+        exit();
+    }
+?>
