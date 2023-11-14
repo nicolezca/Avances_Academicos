213ff9b962e73f64d1f0c9d255fdab2dd2d4554b
nicolezca <tatilezcano03@gmail.com>
2023-11-14 09:40:02 -0300

    consultas en funcion del curso

---
+++ database/consultaCurso.php
@@ -0,0 +1,47 @@
+<?php
+
+function obtenerNombresAlumnos($conn, $idCurso) {
+    $sql = "SELECT a.nombre, a.apellido FROM cursoalumno ca JOIN alumno a ON ca.ID_Alumno = a.id WHERE ca.ID_Curso = $idCurso";
+    $result = $conn->query($sql);
+
+    $nombresAlumnos = array();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $nombreAlumno = $row['nombre'] . ' ' . $row['apellido'];
+            $nombresAlumnos[] = $nombreAlumno;
+        }
+    }
+
+    return $nombresAlumnos;
+}
+
+function obtenerNombrePreceptor($conn, $idCurso) {
+    $sql = "SELECT p.nombre, p.apellido FROM cursopersonal cp JOIN preceptores p ON cp.ID_Preceptor = p.id WHERE cp.ID_Curso = $idCurso";
+    $result = $conn->query($sql);
+
+    $nombreCompletoPreceptor = '';
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $nombreCompletoPreceptor = $row['nombre'] . ' ' . $row['apellido'];
+    }
+
+    return $nombreCompletoPreceptor;
+}
+
+function obtenerNombreProfesor($conn, $idCurso) {
+    $sql = "SELECT pr.nombre, pr.apellido FROM cursopersonal cp JOIN profesores pr ON cp.ID_Profesor = pr.id WHERE cp.ID_Curso = $idCurso";
+    $result = $conn->query($sql);
+
+    $nombreCompletoProfesor = '';
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $nombreCompletoProfesor = $row['nombre'] . ' ' . $row['apellido'];
+    }
+
+    return $nombreCompletoProfesor;
+}
+
+?>
