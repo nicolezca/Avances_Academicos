0fee1fd4e6aec1cd0c47c956463c4c3fbf518092
nicolezca <tatilezcano03@gmail.com>
2023-11-15 07:19:19 -0300

    se agrego nueva card para la seccion de preceptores

--- partials/navBar.php
+++ partials/navBar.php
@@ -6,6 +6,7 @@
     <nav class="link">
         <div class="cards"><a href="../inicio/home.php"><i class='bx bx-home'></i>INCIO</a></div>
         <div class="cards"><a href="../section/profesores.php"><i class='bx bx-user-circle'></i>PROFESORES</a></div>
+        <div class="cards"><a href="../section/preceptores.php"><i class='bx bx-user-circle'></i>PRECEPTORES</a></div>
         <div class="cards"><a href="../section/alumnos.php"><i class='bx bx-group'></i>ALUMNOS</a></div>
         <div class="cards"><a href="../section/curso.php"><i class='bx bx-chalkboard'></i>CURSO</a></div>
         <div class="cards exit" id="verCardExit"><i class='bx bx-door-open'></i>SALIR</div>
