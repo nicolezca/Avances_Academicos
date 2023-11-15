c252eee4e7a358da0c4276f8ae8fc35e3494d0b3
nicolezca <tatilezcano03@gmail.com>
2023-11-15 04:18:42 -0300

    se agrego consultas sql, array, forms, funcionalidades

--- section/InfoCurso.php
+++ section/InfoCurso.php
@@ -5,19 +5,36 @@ $añoCurso = $_SESSION["año"];
 $divisionCursp = $_SESSION["division"];
 $idCurso = $_SESSION["idCurso"];
 
-
+include('../database/conexion.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombreMateria = $_POST['materiaNombre'];
+
+    $sql = "SELECT id FROM cursomateria WHERE materia= '$nombreMateria' AND ID_Curso = $idCurso";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $idMateria = $row["id"];
+    }
 }
 
-
-include('../database/conexion.php');
 include('../database/consultaCurso.php');
+$preceptores = obtenerNombrePreceptor($conn, $idCurso);
+foreach ($preceptores as $preceptor) {
+    $idPreceptor = $preceptor['id'];
+    $nombreCompletoPreceptor = $preceptor['nombre'];
+};
 
+$profesores = obtenerNombreProfesor($conn, $idCurso);
+foreach ($profesores as $profesor) {
+    $idPreceptor = $profesor['id'];
+    $nombreCompletoProfesor = $profesor['nombre'];
+}
 
-$nombresAlumnos = obtenerNombresAlumnos($conn, $idCurso);
-$nombreCompletoPreceptor = obtenerNombrePreceptor($conn, $idCurso);
-$nombreCompletoProfesor = obtenerNombreProfesor($conn, $idCurso);
+$alumnos = obtenerNombresAlumnos($conn, $idCurso);
+foreach ($alumnos as $alumno) {
+    $idAlumno = $alumno['id'];
+    $nombreAlumno = $alumno['nombre'];
+};
 
 
 ?>
@@ -29,6 +46,50 @@ $nombreCompletoProfesor = obtenerNombreProfesor($conn, $idCurso);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../inicio/css/curso2.css">
     <title><?php echo $nombreMateria, ' ', $añoCurso, ' ', $divisionCursp ?></title>
+    <style>
+        .formularioNotas {
+            position: absolute;
+            top: 0;
+            transition: all .3s ease-in;
+            left: calc(50% - 125px);
+        }
+        .formularioNotas form{
+            background-color: white;
+            padding: 1em;
+            box-shadow: 0 0 15px rgba(225, 255, 225, .3);
+        }
+        .formularioNotas form .closeForm{
+            display: flex;
+            justify-content: end;
+            margin-bottom: .5em;
+        }
+        .closeForm span{
+            color: red;
+        }
+        .formularioNotas label{
+            display: block;
+            color: black;
+            text-align: start;
+            color: gray;
+            font-size: 12px;
+        }
+        .formularioNotas input[type="number"]{
+            padding: 5px;
+            background: none;
+            border: none;
+            outline: none;
+            border-bottom: 1px solid blue;
+            color: blue;
+        }
+        .formularioNotas select{
+            padding: 5px 10px;
+            color: blue;
+        }
+        .formularioNotas input[type="submit"]{
+            display: block;
+            margin-top: 10px;
+        }
+        
     </style>
 </head>
 
@@ -46,21 +107,20 @@ $nombreCompletoProfesor = obtenerNombreProfesor($conn, $idCurso);
     <main class="content">
         <div class="agregar">
             <?php if (!empty($nombreCompletoPreceptor)) : ?>
-                <?php echo '';?>
-            <?php else: ?>
-                <?php echo '<button id="VerFormPersonal" style="padding: 10px 30px;border-radius: 10px;border: none;outline: 1px solid white;background: none;color: white;transition: background .3s ease-in-out;cursor: pointer;">Agregar Personal</button>';?>
+                <?php echo ''; ?>
+            <?php else : ?>
+                <?php echo '<button id="VerFormPersonal" style="padding: 10px 30px;border-radius: 10px;border: none;outline: 1px solid white;background: none;color: white;transition: background .3s ease-in-out;cursor: pointer;">Agregar Personal</button>'; ?>
             <?php endif; ?>
             <button id="VerFormAlumno" style="padding: 10px 30px;border-radius: 10px;border: none;outline: 1px solid white;background: none;color: white;transition: background .3s ease-in-out;cursor: pointer;">Agregar Alumno</button>
             <a href="CursoMateria.php" id="volver">Volver Atras</a>
         </div>
 
-
         <div class="conten_directivos">
             <!-- Preceptor del Curso -->
             <div>
                 <h2>Preceptora</h2>
-                <?php if (!empty($nombreCompletoPreceptor)) : ?>
-                    <p><?php echo $nombreCompletoPreceptor; ?></p>
+                <?php if (!empty($preceptor['nombre'])) : ?>
+                    <p><?php echo $preceptor['nombre']; ?></p>
                 <?php else : ?>
                     <p>No hay datos del preceptor para este curso.</p>
                 <?php endif; ?>
@@ -68,15 +128,15 @@ $nombreCompletoProfesor = obtenerNombreProfesor($conn, $idCurso);
             <!-- Docente del Curso -->
             <div>
                 <h2>Docente</h2>
-                <?php if (!empty($nombreCompletoProfesor)) : ?>
-                    <p><?php echo $nombreCompletoProfesor; ?></p>
+                <?php if (!empty($profesor['nombre'])) : ?>
+                    <p><?php echo $profesor['nombre']; ?></p>
                 <?php else : ?>
                     <p>No hay datos del Docente para este curso.</p>
                 <?php endif; ?>
             </div>
         </div>
         <!-- HTML para mostrar los resultados -->
-        <?php if (!empty($nombresAlumnos)) : ?>
+        <?php if (!empty($alumnos)) : ?>
             <div class="content-tabla">
                 <table class="tabla">
                     <thead>
@@ -87,11 +147,41 @@ $nombreCompletoProfesor = obtenerNombreProfesor($conn, $idCurso);
                         </tr>
                     </thead>
                     <tbody>
-                        <?php for ($i = 0; $i < count($nombresAlumnos); $i++) : ?>
+                        <?php for ($i = 0; $i < count($alumnos); $i++) : ?>
                             <tr>
-                                <td><?php echo $nombresAlumnos[$i]; ?></td>
-                                <td><button onclick="subirNota(<?php echo $idAlumnos[$i]; ?>)" style="background-color:blue;">Subir Nota</button></td>
-                                <td><button onclick="verNotas(<?php echo $idAlumnos[$i]; ?>)" style="background-color:white;color:blue;">Ver Notas</button></td>
+                                <td><?php echo $alumno['nombre']; ?></td>
+                                <td>
+                                    <button onclick="subirNota(<?php echo $alumno['id']; ?>)" style="background-color:blue;">Subir Nota</button>
+                                    <div id="formulario_<?php echo $alumno['id']; ?>" class="formularioNotas" style="display:none;">
+                                        <!-- Formulario de subir nota -->
+                                        <form action="procesarNota.php" method="post">
+                                            <div class="closeForm">
+                                                <span id="close">X</span>
+                                            </div>
+                                            <!-- envio de datos invisibles -->
+                                            <input type="hidden" name="idMateria" value="<?php echo $idMateria; ?>">
+                                            <input type="hidden" name="idAlumno" value="<?php echo $alumno['id']; ?>">
+                                            <input type="hidden" name="idCurso" value="<?php echo $idCurso; ?>">
+                                            <input type="hidden" name="idPreceptor" value="<?php echo $preceptor['id']; ?>">
+                                            <input type="hidden" name="idProfesor" value="<?php echo $profesor['id']; ?>">
+                                            <!-- datos visibles -->
+                                            <label for="nota">Ingrese la Nota</label>
+                                            <input type="number" name="nota" id="nota" required placeholder="de 1 a 10">
+                                            <select name="tipoNota" id="tipoNota">
+                                                <option value="Trimestral">Trimestral</option>
+                                                <option value="Parcial">Parcial</option>
+                                                <option value="Final">Final</option>
+                                            </select>
+                                            <input type="submit" value="Subir Nota">
+                                        </form>
+                                    </div>
+                                </td>
+                                <td>
+                                    <form action="" method="post">
+                                        <input type="hidden" name="idCurso" value="<?php echo $alumno['id']; ?>">
+                                        <input type="submit" value="Ver Notas">
+                                    </form>
+                                </td>
                             </tr>
                         <?php endfor; ?>
                     </tbody>
@@ -162,7 +252,14 @@ $nombreCompletoProfesor = obtenerNombreProfesor($conn, $idCurso);
         <input type="hidden" name="idCurso" value="<?php echo $idCurso ?>">
         <input type="submit" value="Enviar">
     </form>
+
     <script src="../inicio/js/curso.js"></script>
+    <script>
+        function subirNota(idAlumno) {
+            var formulario = document.getElementById('formulario_' + idAlumno);
+            formulario.style.display = 'block';
+        }
+    </script>
 </body>
 
 </html>
