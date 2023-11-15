0a288d030c59012d3e9039f145a509d6ed853e16
nicolezca <tatilezcano03@gmail.com>
2023-11-15 07:19:01 -0300

    seccion para la muestra de datos de los preceptores y carga de ellos

---
+++ section/preceptores.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+if (!isset($_SESSION['nombre'])) {
+    header('Location: ../index.html');
+    exit();
+}
+
+include('../database/conexion.php');
+$sql = 'SELECT * FROM preceptores';
+$result = $conn->query($sql);
+
+$preceptores = array(); // Creamos un arreglo para almacenar los datos de los preceptores
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $idPreceptores[] = $row['id'];
+        $nombres[] = $row['nombre'];
+        $apellidos[] = $row['apellido'];
+        $correos[] = $row['correo'];
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../inicio/css/general.css">
+    <title>home | Industrial</title>
+</head>
+
+<body>
+    <!-- barra de navegacion incluida de partiels -->
+    <?php include('../partials/navBar.php'); ?>
+
+    <!-- contenedor principal -->
+    <div class="container" id="container">
+    <?php include('../partials/sesionCerrar.php');?>
+        <nav id="filterNav">
+            <div class="agregar">
+                <button id="verformulario">+</button>
+            </div>
+            <div class="filtrar">
+                <i class='bx bx-filter-alt'></i>
+                <input type="search" name="filter" class="filter" placeholder="buscar nombre">
+                <button id="aplicarFiltro">Aplicar Filtro</button>
+            </div>
+        </nav>
+        <?php if (isset($idPreceptores) && count($idPreceptores) > 0) : ?>
+            <table class="tabla">
+                <thead>
+                    <tr> <!-- Abre una fila de encabezados -->
+                        <th>Identificación</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Correo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($idPreceptores as $key => $id) : ?>
+                        <tr>
+                            <td><?php echo $id; ?></td>
+                            <td><?php echo $nombres[$key]; ?></td>
+                            <td><?php echo $apellidos[$key]; ?></td>
+                            <td><?php echo $correos[$key]; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else : ?>
+            <!-- Mostrar un mensaje si no hay datos -->
+            <div class="mensaje" style="text-align: center;">
+                <p>No se han cargado ningún preceptor</p>
+            </div>
+        <?php endif; ?>
+
+        <form action="../../../avance_academico/database/subirProfesores.php" method="post" id="formSubir" class="formulario">
+            <label for="nombre">Nombre del Preceptor:</label>
+            <input type="text" id="nombre" name="nombre" required autocomplete="off"><br><br>
+
+            <label for="apellido">Apellidos del Preceptor:</label>
+            <input type="text" id="apellido" name="apellido" required autocomplete="off"><br><br>
+
+            <label for="correo">Correo del Preceptor:</label>
+            <input type="text" id="correo" name="correo" required autocomplete="off"><br><br>
+            <input type="submit" value="Agregar">
+        </form>
+    </div>
+
+    <script src="../inicio/js/card.js"></script>
+    <script src="../inicio/js/filter.js"></script>
+    <script src="../inicio/js/forms.js"></script>
+</body>
+
+</html>
